a7f59ee3cbad60fe6f9c1f54dc24d29c2451f2ad
Ernest <em123nest@gmail.com>
2023-05-18 20:38:18 +0100

    project collaboration

--- src/main/java/Neat/and/Tidy/Solutions/cleaning/service/app/service/impl/AdminServiceImpl.java
+++ src/main/java/Neat/and/Tidy/Solutions/cleaning/service/app/service/impl/AdminServiceImpl.java
@@ -7,4 +7,33 @@ import Neat.and.Tidy.Solutions.cleaning.service.app.service.AdminService;
 public class AdminServiceImpl implements AdminService {
 
 
+    @Override
+    public ViewCleanerHistoryResponse viewCleanerHistory(ViewHistoryRequest viewHistoryRequest) {
+        return null;
+    }
+
+    @Override
+    public ViewCustomerHistoryResponse viewCustomerHistory(ViewCustomerHistoryRequest viewCustomerHistoryRequest) {
+        return null;
+    }
+
+    @Override
+    public ApproveCleanerResponse approveCleaner(ApproveCleanerRequest approveCleanerRequest) {
+        return null;
+    }
+
+    @Override
+    public RejectCleanerResponse rejectCleaner(RejectCleanerRequest rejectCleanerRequest) {
+        return null;
+    }
+
+    @Override
+    public ReviewFeedbacksResponse reviewFeedbacks(ReviewFeedbacksRequest reviewFeedbacksRequest) {
+        return null;
+    }
+
+    @Override
+    public void deleteCleaner(DeleteCleanerRequest deleteCleanerRequest) {
+
+    }
 }
