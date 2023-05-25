0a6460b1a7027a58b17736fc0dcb49c2781983e8
Sam Abiodun <samuelabiodun324@gmail.com>
2023-05-25 17:13:59 +0100

    sam_commit

---
+++ src/main/java/Neat/and/Tidy/Solutions/cleaning/service/app/config/appConfig/AppConfig.java
@@ -0,0 +1,2 @@
+package Neat.and.Tidy.Solutions.cleaning.service.app.config.appConfig;public class AppConfig {
+}
--- src/main/java/Neat/and/Tidy/Solutions/cleaning/service/app/service/AdminService.java
+++
@@ -1,8 +0,0 @@
-package Neat.and.Tidy.Solutions.cleaning.service.app.service;
-
-import Neat.and.Tidy.Solutions.cleaning.service.app.data.dto.request.*;
-import Neat.and.Tidy.Solutions.cleaning.service.app.data.dto.response.*;
-
-//public interface AdminService {
-//
-//}
