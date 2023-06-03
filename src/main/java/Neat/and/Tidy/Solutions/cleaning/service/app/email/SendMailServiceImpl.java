3d047c3dbb3f475943181a03a62dd52197b6bcd5
Ernest <em123nest@gmail.com>
2023-06-03 19:21:20 +0100

    updated admin commit

--- src/main/java/Neat/and/Tidy/Solutions/cleaning/service/app/email/SendMailServiceImpl.java
+++ src/main/java/Neat/and/Tidy/Solutions/cleaning/service/app/email/SendMailServiceImpl.java
@@ -24,11 +24,14 @@ public class SendMailServiceImpl implements SendMailService {
         EmailRequest emailRequest = EmailRequest.builder()
                 .to(List.of(new Recipient(request.getRecipientName(), request.getRecipientEmail())))
                 .subject("Invitation to be an Admin of NTJS Cleaning Company")
-                .htmlContent(" We are pleased to invite you to become an admin of the cleaning service industry." +
-                        " As an admin, you will have the ability to manage users, create and edit cleaning jobs, and track the progress of cleaning jobs." +
-                        " To accept this invitation, please click on the following link: " +
-                        "http://localhost:8080/admin/accept-invitation. Thank you for your time and consideration." +
-                        "Sincerely, The NTSJ Cleaning Service Industry Team")
+                .htmlContent(""" 
+                        We are pleased to invite you to become an admin of the cleaning service industry.
+                        As an admin, you will have the ability to manage users, create and edit cleaning jobs, and track the progress of cleaning jobs.
+                        To accept this invitation, please click on the following link:
+                        http://localhost:8080/admin/accept-invitation. \s
+                        Thank you for your time and consideration.
+                        Sincerely, The NTSJ Cleaning Service Industry Team.
+                        """)
                 .build();
         //emailRequest.getTo().add(new Recipient(request.getRecipientName(), request.getRecipientEmail()));
 
